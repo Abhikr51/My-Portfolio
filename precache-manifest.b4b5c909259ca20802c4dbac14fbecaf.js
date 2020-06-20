8769d07c1453127ee2eea41233725f4808714512
Abhijit Kumar <55131747+Abhikr51@users.noreply.github.com>
2020-06-20 20:41:53 +0530

    first commit

---
+++ precache-manifest.b4b5c909259ca20802c4dbac14fbecaf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42beef03b89c20a7cbed7b039f57d6c1",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "2697ad1875eea1eb24d0",
+    "url": "/My-Portfolio/static/css/main.5b7cb8a1.chunk.css"
+  },
+  {
+    "revision": "4e55ce45dbf730272631",
+    "url": "/My-Portfolio/static/js/2.f5426843.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-Portfolio/static/js/2.f5426843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2697ad1875eea1eb24d0",
+    "url": "/My-Portfolio/static/js/main.99f9e3a1.chunk.js"
+  },
+  {
+    "revision": "520fbeebe31e875a2fc5",
+    "url": "/My-Portfolio/static/js/runtime-main.c35249ce.js"
+  },
+  {
+    "revision": "c45aac03eb3a617ff38d84a5989c1447",
+    "url": "/My-Portfolio/static/media/avtarpic.c45aac03.jpg"
+  },
+  {
+    "revision": "f2907b75a9e663c01e9453e66d197eb8",
+    "url": "/My-Portfolio/static/media/poster2.f2907b75.jpg"
+  }
+]);
